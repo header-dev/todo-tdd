2edea8c209cccf2b28020767a02c9d6040fcc0bc
Wiz Tech <headerdev@gmail.com>
2023-10-06 23:10:29 +0700

    Add integration test for GET /todos

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const todoController = require('../controllers/todoController');
 const router = express.Router();
 
+router.get('/', todoController.getTodos);
 router.post('/', todoController.createTodo);
 
 module.exports = router;
