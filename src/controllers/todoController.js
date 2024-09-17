83148be261ae5b274b2a44755660230a78cdcb59
Kritawit.Ya <headerdev@gmail.com>
2024-09-17 22:11:46 +0700

    HTTP Get on /todos/:todoId - should do error handling

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -17,3 +17,12 @@ exports.getTodos = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getTodoById = async (req, res, next) => {
+  try {
+    const todoModel = await TodoModel.findById(req.params.todoId);
+    res.status(200).json(todoModel);
+  } catch (error) {
+    next(error);
+  }
+};
